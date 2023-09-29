b9e4ad7376bf4c6b382c216546d42928d20d00b4
jduriu <jduriu@gmail.com>
2023-09-28 17:36:36 -0700

    revise CalendarGrid and pass additional props for changing date - revised date utility functions to correctly update date arrays - bound date to a useEffect in order to update state on date change

--- app/calendar/page.jsx
+++ app/calendar/page.jsx
@@ -10,21 +10,29 @@ export default function Calendar() {
   const today = new Date()
   const [date, setDate] = useState(today)
   const [calendarInfo, setCalendarInfo] = useState(getCalendarInfo(date))
-  const [day, setDay] = useState(calendarInfo.day)
-  const [month, setMonth] = useState(calendarInfo.month)
-  const [year, setYear] = useState(calendarInfo.year)
+  const [selected, setSelected] = useState(date.getDate())
+
+  useEffect(() => {
+    setCalendarInfo(getCalendarInfo(date))
+    setSelected(date.getDate())
+  }, [date])
+
 
   return (
-
       <div className="flex flex-col m-10">
         <div className="flex py-3 px-2 justify-between">
-          <h1 className="text-3xl">{calendarInfo.monthNames[month]} {year}</h1>
+          <h1 className="text-3xl">{calendarInfo.monthName} {calendarInfo.year}</h1>
           <div>
             <Pagination/>
           </div>
         </div>
         <div className="py-3 px-2 h-full">
-          <CalendarGrid calendarInfo={calendarInfo}/>
+          <CalendarGrid
+          calendarInfo={calendarInfo}
+          setSelected={setSelected}
+          selected={selected}
+          setDate={setDate}
+          />
         </div>
       </div>
 
--- components/CalendarGrid.jsx
+++ components/CalendarGrid.jsx
@@ -1,12 +1,25 @@
-export default function CalendarGrid({calendarInfo}) {
+export default function CalendarGrid({calendarInfo, setSelected, selected, setDate}) {
 
     const dayNames = calendarInfo.daysOfWeek
-
+    const year = calendarInfo.year
+    const month = calendarInfo.month
     const prevMonthDays = calendarInfo.prevMonthDays
     const curMonthDays = calendarInfo.curMonthDays
     const nextMonthDays = calendarInfo.nextMonthDays
 
-    const days = Array.from("#".repeat(35))
+
+    const changeMonth = (month, day) => {
+        if (month > 11) {
+            const newDate = new Date(year+1, 0, day)
+            setDate(newDate)
+        } else if (month < 0) {
+            const newDate = new Date(year-1, 11, day)
+            setDate(newDate)
+        } else {
+            const newDate = new Date(year, month, day)
+            setDate(newDate)
+        }
+    }
 
     return (
         <div className="p-10 h-full">
@@ -23,30 +36,48 @@ export default function CalendarGrid({calendarInfo}) {
             <div className="flex flex-wrap justify-between">
                 {prevMonthDays.map((da) => (
                     <div
-                    className="flex justify-center w-[14.2%] h-[20%] py-5 text-x"
+                    className="flex justify-center w-[14.2%] py-5 text-x"
                     key={da}
+                    onClick={() => changeMonth(month-1, da)}
                     >
-                        <button className="border rounded-full w-[40px] h-[40px] text-blue-600 hover:bg-blue">
+                        <button
+                        className="border rounded-full w-[40px] h-[40px] text-gray-500 hover:bg-slate-200 hover:text-black"
+                        >
                             {da}
                         </button>
                     </div>
                 ))}
                 {curMonthDays.map((da) => (
                     <div
-                    className="flex justify-center w-[14.2%] h-[20%] py-5 text-x"
+                    className="flex justify-center w-[14.2%] py-5 text-x"
                     key={da}
                     >
-                        <button className="border rounded-full w-[40px] h-[40px] text-white hover:bg-blue">
+                        <button
+                        className={
+                            `border rounded-full w-[40px] h-[40px] text-white hover:bg-slate-200 hover:text-black
+                            ${
+                                da === selected
+                                ? 'bg-blue-400'
+                                : ''
+                            }
+                            `
+                        }
+                        onClick={() => setSelected(da)}
+                        >
                             {da}
                         </button>
+
                     </div>
                 ))}
                 {nextMonthDays.map((da) => (
                     <div
-                    className="flex justify-center w-[14.2%] h-[20%] py-5 text-x"
+                    className="flex justify-center w-[14.2%] py-5 text-x"
                     key={da}
                     >
-                        <button className="border rounded-full w-[40px] h-[40px] text-blue-600 hover:bg-blue-400">
+                        <button
+                        className="border rounded-full w-[40px] h-[40px] text-gray-500 hover:bg-slate-200 hover:text-black"
+                        onClick={() => changeMonth(month+1, da)}
+                        >
                             {da}
                         </button>
                     </div>
